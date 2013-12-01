a54ba8323d99c97098c0a0b30d97f47ed391fe47
Koonwah <cadmuxe@gmail.com>
2013-12-01 14:57:38 -0500

    modified:   antonym.py

--- antonym.py
+++ antonym.py
@@ -1,17 +1,29 @@
 from nltk.corpus import wordnet as wn
 
-def antonym(word1, word2):
+def isantonym(word1, word2):
     """
         judge whether two word are antonym
     """
 
     w1_lemma = [lemma for lemma in wn.lemmas(word1)]
-    w1_antonyms = []
+    w1_antonyms = set()
     for lemma in w1_lemma:
         for antonym in lemma.antonyms():
-            w1_antonyms.append(antonym.name)
+            w1_antonyms.add(antonym.name)
 
     if word2 in w1_antonyms:
         return True
     else:
         return False
+
+def antonyms(word):
+    """
+        return all antonyms of word
+    """
+
+    lemmas = [lemma for lemma in wn.lemmas(word)]
+    antonyms = set()
+    for lemma in lemmas:
+        for antonym in lemma.antonyms():
+            antonyms.add(antonym.name)
+    return antonyms
